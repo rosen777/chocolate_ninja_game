3210fe3c77073597c4a36d061128b27b8b455eb5
rosen777 <rosen.toshev@flatironschool.com>
2018-12-31 16:48:00 +0200

    Refactored restart function

--- game.js
+++ game.js
@@ -102,46 +102,5 @@ function collectChocolate (player, chocolate) {
 }
 
 function restart() {
-  game.physics.startSystem(Phaser.Physics.ARCADE)
-
-  game.add.sprite(0, 0, 'sky')
-
-
-  platforms = game.add.group()
-
-  platforms.enableBody = true
-
-  let ground = platforms.create(0, game.world.height - 64, 'ground')
-  ground.scale.setTo(2, 2)
-  ground.body.immovable = true
-
-  let ledge = platforms.create(400, 450, 'ground')
-  ledge.body.immovable = true
-
-  ledge = platforms.create(-75, 350, 'ground')
-  ledge.body.immovable = true
-
-  player = game.add.sprite(32, game.world.height - 150, 'ninja')
-  game.physics.arcade.enable(player)
-  player.body.bounce.y = 0.2
-  player.body.gravity.y = 800
-  player.body.collideWorldBounds = true
-
-  player.animations.add('left', [0, 1, 2, 3], 10, true);
-  player.animations.add('right', [5, 6, 7, 8], 10, true);
-
-
-  chocolates = game.add.group()
-  chocolates.enableBody = true
-
-  for(var i = 0; i < 12; i++) {
-    let chocolate = chocolates.create(i * 70, 0, 'chocolate')
-    chocolate.body.gravity.y = 1000
-    chocolate.body.bounce.y = 0.3 + Math.random() * 0.2
-  }
-
-  scoreText = game.add.text(16, 16, '', {fontSize: '32px', fill: '#b1572c'})
-
-  cursors = game.input.keyboard.createCursorKeys()
-
+  create()
 }
